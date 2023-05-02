11576241f03f41b71ce464da2ae01631bbd56cbf
mshubh <200280107045@ldceahm.gujgov.edu.in>
2023-05-02 16:06:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.cupcake.ui.theme.CupcakeTheme
+
+/**
+ * Activity for cupcake order flow.
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                CupcakeApp()
+            }
+        }
+    }
+}
